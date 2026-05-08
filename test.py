0e379e34f1383d76a4d13e7ef28c34a23f4c6689
Nityakalyani07 <158753924+Nityakalyani07@users.noreply.github.com>
2026-05-08 14:44:30 +0530

    Add multiplication print statement to test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ Build executed at Fri May  8 06:17:56 UTC 2026
 a = 10
 b = 40
 print (a+b)
-
+print (a*b)
 Build executed at Fri May  8 06:30:14 UTC 2026
 Build executed at Fri May  8 07:47:39 UTC 2026
 Build executed at Fri May  8 09:04:01 UTC 2026
